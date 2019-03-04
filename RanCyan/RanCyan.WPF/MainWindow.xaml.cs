a75a96848d1a0030c94456a011e1059abf7b7f95
放浪 軍師 <roamschemer@gmail.com>
2019-03-05 01:17:48 +0900

    新規作り直し開始。PrismとWPFを導入。

---
+++ RanCyan/RanCyan.WPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism;
+using Prism.Ioc;
+
+namespace RanCyan.WPF
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Xamarin.Forms.Platform.WPF.FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new RanCyan.App(new WpfInitializer()));
+        }
+        public class WpfInitializer : IPlatformInitializer
+        {
+            public void RegisterTypes(IContainerRegistry containerRegistry)
+            {
+                // Register any platform specific implementations
+            }
+        }
+    }
+}
