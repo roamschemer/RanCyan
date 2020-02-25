0e300ad54ad4698e2c911f375c7119f2b7b2e5b7
放浪 軍師 <roamschemer@gmail.com>
2020-02-25 10:29:40 +0900

    LotteryCategorySelectionViewModel を追加して接続完了まで

---
+++ RanCyan/RanCyan/ViewModels/LotteryCategorySelectionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RanCyan.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RanCyan.ViewModels {
+    /// <summary>
+    /// 各カテゴリ選択用ColectionViewの中身
+    /// </summary>
+    public class LotteryCategorySelectionViewModel {
+        public LotteryCategorySelectionViewModel(LotteryCategoryModel lotteryCategoryModel) {
+
+        }
+    }
+}
--- RanCyan/RanCyan/ViewModels/LotteryPageViewModel.cs
+++ RanCyan/RanCyan/ViewModels/LotteryPageViewModel.cs
@@ -11,9 +11,11 @@ using System.Linq;
 namespace RanCyan.ViewModels {
     public class LotteryPageViewModel : ViewModelBase {
         public ReactiveProperty<string> Title { get; }
+        public ReadOnlyReactiveCollection<LotteryCategorySelectionViewModel> LotteryCategorySelectionViewModels { get; }
         public LotteryPageViewModel(INavigationService navigationService, CoreModel coreModel) : base(navigationService) {
             var lotteryPageModel = coreModel.LotteryPageModel;
             Title = lotteryPageModel.ObserveProperty(x => x.Title).ToReactiveProperty().AddTo(this.Disposable);
+            LotteryCategorySelectionViewModels = lotteryPageModel.CategoryModels.ToReadOnlyReactiveCollection(x => new LotteryCategorySelectionViewModel(x));
         }
     }
 }
