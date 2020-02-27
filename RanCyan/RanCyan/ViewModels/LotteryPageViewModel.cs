5aec679e7770bfbfeec64eb4d09139c3b655bc44
roamschemer <roamschemer@gmail.com>
2020-02-27 10:08:51 +0900

    LotteryPageViewModel を 一部調整

--- RanCyan/RanCyan/ViewModels/LotteryPageViewModel.cs
+++ RanCyan/RanCyan/ViewModels/LotteryPageViewModel.cs
@@ -28,21 +28,23 @@ namespace RanCyan.ViewModels {
 
         public LotteryPageViewModel(INavigationService navigationService, CoreModel coreModel) : base(navigationService) {
             //乱ちゃんイメージ
-            coreModel.RanCyanModel.StartRancyanImage();
-            RanCyanImage = coreModel.RanCyanModel.ObserveProperty(x => x.RanCyanImage).ToReactiveProperty().AddTo(this.Disposable);
-            IsImageActive = coreModel.RanCyanModel.ObserveProperty(x => x.IsImageActive).ToReactiveProperty().AddTo(this.Disposable);
+            var ranCyanModel = coreModel.RanCyanModel;
+            ranCyanModel.StartRancyanImage();
+            RanCyanImage = ranCyanModel.ObserveProperty(x => x.RanCyanImage).ToReactiveProperty().AddTo(this.Disposable);
+            IsImageActive = ranCyanModel.ObserveProperty(x => x.IsImageActive).ToReactiveProperty().AddTo(this.Disposable);
             var imageTimer = new ReactiveTimer(TimeSpan.FromSeconds(10));
             imageTimer.Subscribe(_ => {
-                coreModel.RanCyanModel.WaitingRancyanImage();
+                ranCyanModel.WaitingRancyanImage();
                 imageTimer.Stop();
             });
             imageTimer.Start(TimeSpan.FromSeconds(0.5));
-            //表示系
+            //ContentView
             var lotteryPageModel = coreModel.SelectionLotteryPageModel;
-            Title = lotteryPageModel.ObserveProperty(x => x.Title).ToReactiveProperty().AddTo(this.Disposable);
             LotteryCategorySelectionViewModels = lotteryPageModel.LotteryCategoryModels.ToReadOnlyReactiveCollection(x => new LotteryCategorySelectionViewModel(coreModel, x)).AddTo(this.Disposable);
+            //表示系
+            Title = lotteryPageModel.ObserveProperty(x => x.Title).ToReactiveProperty().AddTo(this.Disposable);
             SelectionViewWidth = coreModel.ObserveProperty(x => x.SelectionViewWidth).ToReactiveProperty().AddTo(this.Disposable);
-            CategoryModelsCount = lotteryPageModel.ObserveProperty(x=>x.CategoryModelsCount).ToReactiveProperty().AddTo(this.Disposable);
+            CategoryModelsCount = lotteryPageModel.ObserveProperty(x => x.CategoryModelsCount).Select(x => (x > 0) ? x : 1).ToReactiveProperty().AddTo(this.Disposable);
             SelectionViewOneWidth = SelectionViewWidth.CombineLatest(CategoryModelsCount, (x, y) => x / y).ToReadOnlyReactiveProperty().AddTo(this.Disposable);
             LotteryLabel = lotteryPageModel.ObserveProperty(x => x.LotteryLabelText).ToReactiveProperty().AddTo(this.Disposable);
             LotteryLabelColor = lotteryPageModel.ObserveProperty(x => x.LotteryLabelColor).ToReactiveProperty().AddTo(this.Disposable);
